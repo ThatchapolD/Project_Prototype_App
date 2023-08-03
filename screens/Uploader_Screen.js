6c3857bfa0f06b5b67467d07a7fbf437a34e14bb
Thatchapol D <thatchapol@td1932prv.net>
2023-08-03 19:10:22 +0700

    Creating two screen Tutorial and Uploader_Screen

---
+++ screens/Uploader_Screen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+function Uploader_Screen() {
+  return (
+    <View>
+      <Text>Uploader_Screen</Text>
+    </View>
+  );
+}
+
+export default Uploader_Screen;
+
+const styles = StyleSheet.create({});
