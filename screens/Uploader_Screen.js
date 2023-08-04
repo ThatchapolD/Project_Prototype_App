b6a6a1d91a9d87dba4ae1203f8f2b86e42ba3a6d
Thatchapol D <thatchapol@td1932prv.net>
2023-08-04 19:27:47 +0700

    Adding A HTTP Post to Node-Red

--- screens/Uploader_Screen.js
+++ screens/Uploader_Screen.js
@@ -1,5 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import axios from "axios";
+import * as ImagePicker from "expo-image-picker";
+
+//Import Icon component
 import { FontAwesome } from "@expo/vector-icons";
 
 //Import other component
@@ -8,15 +12,49 @@ import MainButton from "../UI/MainButton";
 //Importing Style Color
 import { GlobalColor } from "../style/Color";
 
-function handleUpload() {
-  console.log("Pressed!!!");
-}
+const selectImage = async () => {
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 0,
+  });
+
+  if (!result.canceled) {
+    // Now, send the image to the server using HTTP
+    handleUpload(result);
+  }
+};
+
+const handleUpload = async (imageData) => {
+  const formData = new FormData();
+  formData.append("image", {
+    uri: imageData.uri,
+    type: imageData.type,
+    name: imageData.fileName,
+  });
+  try {
+    const response = await axios.post(
+      "http://localhost:1880/uploadimage",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
+    );
+
+    console.log("Image uploaded successfully!", response.data);
+  } catch (error) {
+    console.error("Error uploading image:", error);
+  }
+};
 
 function Uploader_Screen() {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <MainButton onPressed={handleUpload}>
+        <MainButton onPressed={selectImage}>
           Please upload 1 picture
           <View style={styles.pictureContainer}>
             <FontAwesome name="file-picture-o" size={24} color="black" />
