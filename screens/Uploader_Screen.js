dfe2f40e46fdadf1b5aafc687689c500443915b2
Thatchapol D <thatchapol@td1932prv.net>
2023-09-12 14:06:59 +0700

    Finally can upload the image to server

--- screens/Uploader_Screen.js
+++ screens/Uploader_Screen.js
@@ -1,8 +1,7 @@
 import { StyleSheet, View } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as ImagePicker from "expo-image-picker";
-import * as ImageManipulator from "expo-image-manipulator";
 
 //Import Icon component
 import { FontAwesome } from "@expo/vector-icons";
@@ -17,53 +16,75 @@ import { GlobalColor } from "../style/Color";
 import Config from "../assets/Config";
 
 function Uploader_Screen() {
-  const handleUpload = async (imageData) => {
-    try {
-      // Convert the image to base64
-      const base64Image = imageData;
+  const [selectedImage, setSelectedImage] = useState(null);
 
-      // Send the base64 image data to the server
-      const response = await axios.post(
-        "http://" + Config.Node_RED_IP + Config.Upload, //Change the IP in Config file
-        { image: base64Image },
-        {
-          headers: {
-            "Content-Type": "application/json",
+  useEffect(() => {
+    if (selectedImage) {
+      console.log("Selected image:", selectedImage);
+      uploadImage(selectedImage);
+    }
+  }, [selectedImage]);
+
+  const uploadImage = async () => {
+    if (selectedImage) {
+      const formData = new FormData();
+      formData.append("image", {
+        uri: selectedImage,
+        name: "image.jpg",
+        type: "image/jpg",
+      });
+
+      try {
+        const response = await axios.post(
+          "http://192.168.43.220:256/uploadimage",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
           },
-        },
-      );
+        );
 
-      console.log("Image uploaded successfully!", response.data);
-    } catch (error) {
-      console.error("Error uploading image:", error);
+        console.log("Image uploaded successfully:", response.data);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    } else {
+      console.warn("No image selected.");
     }
   };
 
   const selectImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      // Now, send the image to the server using HTTP
-      convertImageToBase64(result.uri);
-    }
-  };
-
-  const convertImageToBase64 = async (imageUri) => {
     try {
-      const manipResult = await ImageManipulator.manipulateAsync(
-        imageUri,
-        [{ resize: { width: 200 } }],
-        { base64: true },
-      );
-      //Send the data into handleUpload
-      handleUpload(`data:image/jpeg;base64,${manipResult.base64}`);
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      console.log("Permission status:", status);
+
+      if (status !== "granted") {
+        console.warn("Permission to access image library is not granted.");
+        return;
+      }
+
+      const { assets } = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (assets.length === 0) {
+        console.warn("No image selected.");
+        return;
+      }
+
+      const selectedImageUri = assets[0].uri;
+      if (selectedImageUri) {
+        setSelectedImage(selectedImageUri);
+      } else {
+        console.warn("No valid image URI found.");
+      }
     } catch (error) {
-      console.error("Error converting image to base64:", error);
+      console.error("Error while picking image:", error);
     }
   };
 
