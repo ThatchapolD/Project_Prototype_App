66fb68b241f501fb35acda09ded96e2a61f56a04
Thatchapol D <thatchapol@td1932prv.net>
2023-09-16 22:00:49 +0700

    Change  + to  in HTTP method

--- screens/Uploader_Screen.js
+++ screens/Uploader_Screen.js
@@ -36,7 +36,7 @@ function Uploader_Screen() {
 
       try {
         const response = await axios.post(
-          "http://" + Config.Mac_IP + Config.Port + Config.Upload,
+          `http://${Config.Mac_IP}:${Config.Port}${Config.Upload}`,
           formData,
           {
             headers: {
